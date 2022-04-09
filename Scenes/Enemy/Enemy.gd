0380a88ce7ca4700e9ac2d5211daf69ed43abf16
Norman B. Lancaster <qbradq@gmail.com>
2022-04-09 17:49:18 -0500

    Added enemy droping powerups

--- Scenes/Enemy/Enemy.gd
+++ Scenes/Enemy/Enemy.gd
@@ -3,6 +3,11 @@ extends Area2D
 var sparks_scene:PackedScene = load("res://Scenes/Bits/Sparks.tscn")
 
 export var speed:float = 50.0
+export var drop_chance:float = 5.0
+
+var powerups = [
+	load("res://Scenes/PowerUp/PowerUp.tscn")
+]
 
 var velocity:Vector2
 
@@ -14,15 +19,21 @@ func _physics_process(delta):
 	global_position += velocity * delta
 
 func die():
+	# Play the pow sound effect
 	var pa = $PowAudio
 	if pa != null:
 		remove_child(pa)
 		get_parent().find_node("AudioTemp").add_child(pa)
 		pa.play()
+	# Create sparks explosion
 	var sparks = sparks_scene.instance()
 	sparks.global_position = global_position
 	sparks.restart()
 	get_parent().add_child(sparks)
+	# Drop power-ups
+	if rand_range(0.0, 100.0) < drop_chance:
+		drop()
+	# And remove ourselves
 	queue_free()
 
 func look_at_player():
@@ -33,3 +44,8 @@ func look_at_player():
 func seek_player():
 	look_at_player()
 	velocity = Vector2.RIGHT.rotated(rotation) * speed
+
+func drop():
+	var pu = powerups[randi()%powerups.size()].instance()
+	pu.global_position = global_position
+	get_parent().add_child(pu)
