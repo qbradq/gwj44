cea79fc8812a35d79bc410a330dea0d7eeb8e7d8
Norman B. Lancaster <qbradq@gmail.com>
2022-04-13 01:53:12 -0500

    Minor refactors

--- Scenes/Level/Level.gd
+++ Scenes/Level/Level.gd
@@ -5,19 +5,19 @@ func _ready():
 
 func _process(delta):
 	# Score
-	$ScoreLabel.text = "SCORE: %d" % LevelData.get_score()
+	$UILayer/ScoreLabel.text = "SCORE: %d" % LevelData.get_score()
 	# Ammo
 	if $Player and $Player/Gun:
 		if $Player/Gun.infinite_ammo:
-			$AmmoLabel.text = "AMMO: ∞"
+			$UILayer/AmmoLabel.text = "AMMO: ∞"
 		else:
-			$AmmoLabel.text = "AMMO: %d" % $Player/Gun.ammo
+			$UILayer/AmmoLabel.text = "AMMO: %d" % $Player/Gun.ammo
 	# Time
 	LevelData.increment_time(delta)
 	var ltime = LevelData.get_time_elapsed()
 	var minutes = ltime / 60
 	var seconds = ltime % 60
-	$TimeLabel.text = "TIME: %d:%02d" % [minutes, seconds]
+	$UILayer/TimeLabel.text = "TIME: %d:%02d" % [minutes, seconds]
 
 func _physics_process(delta):
 	# Clean up temp audio nodes
--- Scenes/UI/MuteButton.gd
+++ Scenes/UI/MuteButton.gd
@@ -1,7 +1,14 @@
 extends TextureButton
 
+func _ready():
+	update_button_texture()
+
 func _on_MuteButton_pressed():
+	$AudioStreamPlayer.play()
 	AudioServer.set_bus_mute(0, not AudioServer.is_bus_mute(0))
+	update_button_texture()
+
+func update_button_texture():
 	if AudioServer.is_bus_mute(0):
 		texture_normal = preload("res://Art/TextureButtons/mute.png")
 	else:
