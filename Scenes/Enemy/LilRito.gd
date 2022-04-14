13a2d4c0ce51f70ef30a520d9446fd074ae7d3ac
Norman B. Lancaster <qbradq@gmail.com>
2022-04-14 01:51:50 -0500

    Added Mountain Dew Rito enemy

--- Scenes/Enemy/LilRito.gd
+++ Scenes/Enemy/LilRito.gd
@@ -1,10 +1,14 @@
 extends "res://Scenes/Enemy/Enemy.gd"
 
+export var seek:bool
+
 func _ready():
 	seek_player()
 
 func _physics_process(delta):
 	# AI processing
+	if seek:
+		seek_player()
 	if not $VisibilityNotifier2D.is_on_screen():
 		queue_free()
 	# Base processing
