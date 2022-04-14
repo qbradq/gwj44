b3f0e23e17520e3b9f48b58b288da97d55c70dfd
Norman B. Lancaster <qbradq@gmail.com>
2022-04-13 22:50:08 -0500

    Added LilRito launching from Rito enemy

--- Scenes/Enemy/LilRito.gd
+++ Scenes/Enemy/LilRito.gd
@@ -1,17 +1,11 @@
 extends "res://Scenes/Enemy/Enemy.gd"
 
-var was_on_screen:bool
-
 func _ready():
 	seek_player()
 
 func _physics_process(delta):
 	# AI processing
-	rotate(2 * PI * delta)
-	if $VisibilityNotifier2D.is_on_screen():
-		was_on_screen = true
-	else:
-		if was_on_screen:
-			queue_free()
+	if not $VisibilityNotifier2D.is_on_screen():
+		queue_free()
 	# Base processing
 	._physics_process(delta)
--- Scenes/Enemy/Rito.gd
+++ Scenes/Enemy/Rito.gd
@@ -1,7 +1,16 @@
 extends "res://Scenes/Enemy/Enemy.gd"
 
+var lilrito_scene = preload("res://Scenes/Enemy/LilRito.tscn")
+
 func _physics_process(delta):
 	# AI processing
 	seek_player()
 	# Base processing
 	._physics_process(delta)
+
+
+func _on_LaunchTimer_timeout():
+	var lilrito:Area2D = lilrito_scene.instance()
+	lilrito.global_transform = $LaunchPoint.global_transform
+	get_parent().add_child(lilrito)
+
